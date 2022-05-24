b1333c673ab9924430522d1982aeb860967af406
WilSh4ke <wilshake11@gmail.com>
2022-05-24 01:44:22 -0300

    Criando loanding

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -44,7 +44,7 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
-          Pedding(
+          Padding(
             padding: EdgeInsets.all(10.0),
             child: TextField(
               decoration: InputDecoration(
@@ -54,10 +54,37 @@ class _HomePageState extends State<HomePage> {
               ),
               style: TextStyle(color: Colors.white, frontSize: 18.0),
               textAlign: TextAlign.center,
-            )
+            ),
+          ),
+          Expanded(
+              child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot){
+                  switch(snapshot.connectionState){
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child:  CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                          default:
+                           if(snapshot.hasError) return Container();
+                           else return _createGifTable(context, snapshot);
+                  }
+                }
+              )
           )
         ],
       )
     );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+
   }
 }
