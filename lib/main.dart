c4fa697cca5ec8a331495679a28f30f64f43be64
WilSh4ke <wilshake11@gmail.com>
2022-05-24 00:26:57 -0300

    criado projeto, requisitando dados

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:gif/main.dart';
+
+import 'package:gif/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+
+}
+
+
